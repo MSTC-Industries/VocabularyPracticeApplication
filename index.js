4d159d7b06b8b9bb94342db063a9b3ca2fb41dde
LawrenceM2867 <hcps-morganla2@henricostudents.org>
2025-04-17 02:08:09 +0000

    lesson # can now be transfered from main to lessons page

---
+++ index.js
@@ -0,0 +1,160 @@
+var lesson_data = { //all of the data for each of the lessons
+    "12" : { //lesson #
+        "words" : { //all of the words
+            "deluge" : { //the word
+                "ps" : "n", //part of speech
+                "def" : "a flood; an overwhelming rush", //defenition
+                "ex" : "The new amusement park experienced a ____ of visitors on opening day.", //example sentence, _ x4 is where the word goes
+                "ant" : ["drought", "dearth"], //antonyms
+                "syn" : ["inundation", "surge"] //synonyms
+            },
+
+            "catholic" : {
+                "ps" : "adj",
+                "def" : "universal; wide-ranging",
+                "ex" : "His ____ interests made him quite knowledgeable in may subjects",
+                "ant" : ["provincial", "limited", "parochial"],
+                "syn" : ["broad"]
+            },
+
+            "eerie" : {
+                "ps" : "adj",
+                "def" : "weird; mysterious; strange and frightening",
+                "ex" : "No one accepted the dare to stay in the ____ old mansion for one night.",
+                "ant" : ["common", "ordinary"],
+                "syn" : ["creepy", "sinister"]
+            },
+
+            "martial" : {
+                "ps" : "adj",
+                "def" : "warlike; relating to the military",
+                "ex" : "A state of ____ law was declared in the small country in the weeks following the overthrow of the government.",
+                "ant" : [],
+                "syn" : []
+            },
+
+            "anthropomorphic" : {
+                "ps" : "adj",
+                "def" : "attributing human characteristics or qualities to objects, animals, or gods",
+                "ex" : "____ stories might feature pigs and rabbits walking upright wearing clothes, and speaking to each other in human languages.",
+                "ant" : [],
+                "syn" : []
+            },
+
+            "beneficiary" : {
+                "ps" : "n",
+                "def" : "one who receives benefits",
+                "ex" : "John was the sole ____ of his Uncle Martin’s vast estate.",
+                "ant" : [],
+                "syn" : ["recipient", "heir"]
+            },
+
+            "careen" : {
+                "ps" : "v",
+                "def" : "to swerve or lurch from side to side while in motion",
+                "ex" : "The torrential winds caused the ship to ____ violently.",
+                "ant" : [],
+                "syn" : ["tilt"]
+            },
+
+            "aplomb" : {
+                "ps" : "n",
+                "def" : "self-confidence",
+                "ex" : "The ____ of the young dancer astonished the veterans of the troupe.",
+                "ant" : ["awkwardness"],
+                "syn" : ["assurance", "poise"]
+            },
+
+            "guile" : {
+                "ps" : "n",
+                "def" : "slyness and cunning in dealing with others",
+                "ex" : "Brad’s ____ contributed to his wealth, but it also created enemies.",
+                "ant" : ["honesty"],
+                "syn" : ["craftiness", "astuteness"]
+            },
+
+            "modicum" : {
+                "ps" : "n",
+                "def" : "a small amount",
+                "ex" : "A sudden shower gave us a ____ of relief from the heat and humidity.",
+                "ant" : ["abundance"],
+                "syn" : ["bit"]
+            },
+
+            "rancid" : {
+                "ps" : "adj",
+                "def" : "having a bad taste ",
+                "ex" : "The bitter fight over child custody left a ____ taste in both lawyers' mouths.",
+                "ant" : ["fresh"],
+                "syn" : ["rotten", "repulsive"]
+            },
+
+            "havoc" : {
+                "ps" : "n",
+                "def" : "great destruction; chaos",
+                "ex" : "The commandos wreaked ____ throughout the area when they infiltrated the secret base.",
+                "ant" : ["order"],
+                "syn" : ["mayhem", "disorder"]
+            },
+
+            "pall" : {
+                "ps" : "n",
+                "def" : "something that covers or conceals",
+                "ex" : "A ____ of gloom descended over the crowd.",
+                "ant" : [],
+                "syn" : ["shroud"]
+            },
+
+            "languish" : {
+                "ps" : "v",
+                "def" : "to become weak or feeble; to lose strength",
+                "ex" : "I ____ed at the thought of all the work that still needed to be done",
+                "ant" : ["thrive"],
+                "syn" : ["wither", "fade"]
+            },
+
+            "fester" : {
+                "ps" : "v",
+                "def" : "to grow embittered over time; to rot",
+                "ex" : "If allowed to ____, dislike can turn into bitter hatred.",
+                "ant" : [],
+                "syn" : ["aggravated", "worsen"]
+            }
+        },
+
+        "context" : { //for all the putting words into context paragraphs
+
+        },
+
+        "rps" : { //roots, prefixes, and suffixes
+            "roots" : { //all the roots
+                "patr" : "father",
+                "patern" : "father",
+                "scop" : "to watch",
+                "scrib" : "to write",
+                "script" : "to write",
+                "cent" : "100",
+            },
+
+            "prefixes" : {}, //all the prefixes
+
+            "suffixes" : {}, //all the suffixes
+
+            "words" : { //words that have the roots, prefixes, and suffixes in them
+
+            },
+
+            "sentences" : { //sentences that have the roots, prefixes, and suffixes in them
+
+            }
+        }
+    }
+}
+
+function index_setLesson(lessonNum) { //sets the lesson # to use
+    sessionStorage.setItem("current_lesson", lessonNum)
+}
+
+function lessons_onload() { //runs when the lessons page opens up
+    document.getElementById("title").innerHTML = "Lesson " + sessionStorage.getItem("current_lesson") //set the title to the lesson
+}
